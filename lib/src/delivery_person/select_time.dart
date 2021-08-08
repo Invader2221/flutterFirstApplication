65cc1239a28c2e77e8008ac65d5d1be5553c0edd
Invader2221 <tharindukasun05@gmail.com>
2021-08-08 18:31:36 +0530

    deliverguy route select

---
+++ lib/src/delivery_person/select_time.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SelectTime extends StatefulWidget {
+  SelectTime({Key key}) : super(key: key);
+
+  @override
+  _SelectTimeState createState() => _SelectTimeState();
+}
+
+class _SelectTimeState extends State<SelectTime> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/delivery_person/select_your_route.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SelectYourRoute extends StatefulWidget {
+  SelectYourRoute({Key key}) : super(key: key);
+
+  @override
+  _SelectYourRouteState createState() => _SelectYourRouteState();
+}
+
+class _SelectYourRouteState extends State<SelectYourRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
